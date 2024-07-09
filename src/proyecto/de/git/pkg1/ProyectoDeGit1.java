d8ac6b62c75354473129105cbc487f0ba856c8c6
Vladimir Rodríguez <jacitoshadow@gmail.com>
2024-07-09 08:05:25 -0600

    se agrego otro ciclo

--- src/proyecto/de/git/pkg1/ProyectoDeGit1.java
+++ src/proyecto/de/git/pkg1/ProyectoDeGit1.java
@@ -1,20 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package proyecto.de.git.pkg1;
 
-/**
- *
- * @author usuario
- */
 public class ProyectoDeGit1 {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("ola");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            
+        }
+        // esto es otro ciclo de prueba
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("");
+        }
     }
     
 }
